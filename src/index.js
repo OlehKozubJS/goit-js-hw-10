17f79558cc976b25d0399ae4f4b6e16ddf25e656
OlehKozubJS <oleg.kozub54@gmail.com>
2023-06-11 23:40:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,45 +15,62 @@ const url = `https://api.thecatapi.com/v1/breeds`;
 const api_key = "live_iTXFzPkHdC4oTgN59kH3GxZuT9eSiCYByP1BBv24lnnQqDZPMjk0iJuaj0bMHA4W";
 let storedBreeds = [];
 
- fetch(url,{headers: {
-      'x-api-key': api_key
-    }})
- .then((response) => {
-   return response.json();
- })
-.then((data) => {
-   
-   //filter to only include those with an `image` object
-   data = data.filter(img=> img.image?.url!=null)
-   
-  storedBreeds = data;
-   
-   for (let i = 0; i < storedBreeds.length; i++) {
-    const breed = storedBreeds[i];
-    let option = document.createElement('option');
-     
-     //skip any breeds that don't have an image
-     if(!breed.image) continue;
-     
-    //use the current array index
-    option.value = i;
-    option.innerHTML = `${breed.name}`;
-breedSelect.appendChild(option);
-    
-    }
-   //show the first breed by default
-   showBreedImage(0);
-})
-.catch(function(error) {
-   console.log(error);
-});
+breedSelect.addEventListener("click", showBreedInfo);
 
-function showBreedImage(index)
-{ 
-  //document.getElementById("breed_image").src= storedBreeds[index].image.url;
-  
-  catInfo.textContent = storedBreeds[index].temperament;
-  
-  //document.getElementById("wiki_link").href= storedBreeds[index].wikipedia_url
-  //document.getElementById("wiki_link").innerHTML= storedBreeds[index].wikipedia_url
+function fetchCatByBreed() {
+   fetch(url, {headers: {
+         'x-api-key': api_key
+   }})
+   .then((response) => {
+      return response.json();
+   })
+   .then((data) => {
+      
+      //filter to only include those with an `image` object
+      data = data.filter(img=> img.image?.url!=null)
+      
+   storedBreeds = data;
+      
+      for (let i = 0; i < storedBreeds.length; i++) {
+      const breed = storedBreeds[i];
+      let option = document.createElement('option');
+      
+      //skip any breeds that don't have an image
+      if(!breed.image) continue;
+      
+      //use the current array index
+      option.value = i;
+      option.innerHTML = `${breed.name}`;
+      breedSelect.appendChild(option);
+      
+      }
+   })
+   .catch(function(error) {
+      console.log(error);
+   });
+}
+
+function showBreedInfo() { 
+  loader.style.display = "none";
+  const selectBreedId = breedSelect.value;
+  fetchCatByBreed(selectBreedId).then(
+   data => {
+      const html = data.map (
+         cat =>
+            `
+            <li>
+               <img src="${cat.url}" alt="cat-image" width="300" />
+               <h2>${cat.breeds[0].name}</h2>
+               <p>${cat.breeds[0].name}</p>
+            </li>
+            `
+      ).join("");
+      catInfo.innerHTML = html;
+   }).catch(() => {
+      console.log("Oops! Something went wrong! Try reloading this page");
+   }).finally(
+      () => {
+         loader.style.display = "none";
+      }
+   );
 }
