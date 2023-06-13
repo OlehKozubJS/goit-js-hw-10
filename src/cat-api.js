b5a72685eb91550ac24c21c62333b8bb9374a9a7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-06-13 18:42:38 +0300

    Add Exeption and Design

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,43 +1,22 @@
-const breedSelect = document.querySelector(".breed-select");
-const loader = document.querySelector(".loader");
-const error = document.querySelector(".error");
-const catInfo = document.querySelector(".cat-info");
-const breedImage = document.querySelector(".breed-image");
-const breedDescription = document.querySelector(".breed-description");
-const breedTemperament = document.querySelector(".breed-temperament");
+export function fetchBreeds(selector, data) {
+    //filter to only include those with an `image` object
+    data = data.filter(img=> img.image?.url!=null)
 
-export function fetchBreeds() {
-    let storedBreeds = [];
+    let storedBreeds = data;
 
-    fetch("https://api.thecatapi.com/v1/breeds", {headers: {
-       "x-api-key": "live_iTXFzPkHdC4oTgN59kH3GxZuT9eSiCYByP1BBv24lnnQqDZPMjk0iJuaj0bMHA4W"
-    }})
-    .then((response) => {
-       return response.json();
-    })
-    .then((data) => {
- 
-       //filter to only include those with an `image` object
-       data = data.filter(img=> img.image?.url!=null)
- 
-       storedBreeds = data;
- 
-       for (let i = 0; i < storedBreeds.length; i++) {
-          const breed = storedBreeds[i];
-          let option = document.createElement('option');
-       
-          //skip any breeds that don't have an image
-          if(!breed.image)continue;
-       
-          //use the current array index
-          option.value = i;
-          option.innerHTML = `${breed.name}`;
-          breedSelect.appendChild(option);
-       }
-       //show the first breed by default  
-       return storedBreeds;
-    })
-    .catch(function() {
-       error.classList.remove("hidden");
-    });
- }
+    for (let i = 0; i < storedBreeds.length; i++) {
+        const breed = storedBreeds[i];
+        let option = document.createElement('option');
+    
+        //skip any breeds that don't have an image
+        if(!breed.image)continue;
+    
+        //use the current array index
+        option.value = i;
+        option.innerHTML = `${breed.name}`;
+        selector.appendChild(option);
+
+    }
+
+    return storedBreeds;
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
+const url = `https://api.thecatapi.com/v1/breeds`;
+const api_key = "live_iTXFzPkHdC4oTgN59kH3GxZuT9eSiCYByP1BBv24lnnQqDZPMjk0iJuaj0bMHA4W";
+
 const breedSelect = document.querySelector(".breed-select");
-const loader = document.querySelector(".loader");
-const error = document.querySelector(".error");
-const catInfo = document.querySelector(".cat-info");
+const loaderEl = document.querySelector(".loader");
+const errorEl = document.querySelector(".error");
 const breedImage = document.querySelector(".breed-image");
 const breedDescription = document.querySelector(".breed-description");
 const breedTemperament = document.querySelector(".breed-temperament");
 
-import {fetchBreeds} from "./cat-api"; 
+window.addEventListener("load", selectBreed);
+breedSelect.addEventListener("change",selectBreed);
 
-window.addEventListener("load", fetchBreeds);
-breedSelect.addEventListener("change", selectBreedFunction);
+import {fetchBreeds} from "./cat-api";
 
-function selectBreedFunction() {
-   let index = breedSelect.value;
+function selectBreed(e) {
+   fetch(url, {headers: {"x-api-key": api_key}})
+      .then((response) => {
+            return response.json();
+         })
+      .then((data) => {
+         errorEl.classList.add("hidden");
+  
+         let storedBreeds = fetchBreeds(breedSelect, data);
 
-   breedImage.src= storedBreeds[index].image.url;  
-   breedDescription.textContent= storedBreeds[index].description;
-   breedTemperament.textContent= storedBreeds[index].temperament;
+         let index = e.type === "load" ? 0 : breedSelect.value;
+
+         breedImage.src= storedBreeds[index].image.url;  
+         breedDescription.textContent= storedBreeds[index].description;
+         breedTemperament.textContent= storedBreeds[index].temperament;
+         })
+      .catch(function(error) {
+         errorEl.classList.remove("hidden");
+         console.log(error);
+      }
+   );
 }
