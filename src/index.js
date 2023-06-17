3e23df49830fc27d98c7b3fb02872a4596d732f6
OlehKozubJS <oleg.kozub54@gmail.com>
2023-06-17 21:06:17 +0300

    GENERAL UPDATE

--- src/index.js
+++ src/index.js
@@ -1,71 +1,67 @@
+import {fetchBreeds} from "./cat-api";
+import {fetchCatByBreed} from "./cat-api";
+
 const breedSelect = document.querySelector(".breed-select");
 const loaderEl = document.querySelector(".loader");
 const errorEl = document.querySelector(".error");
 const catInfo = document.querySelector(".cat-info");
 
-import {fetchBreeds} from "./cat-api";
-import {fetchCatByBreed} from "./cat-api";
+[breedSelect, loaderEl, errorEl, catInfo].forEach((item) => {item.classList.add("hidden")});
 
-window.addEventListener("load", () => fetchBreeds(breedSelect, loaderEl, errorEl));
-breedSelect.addEventListener("change", () => fetchCatByBreed(breedSelect.value));
+window.addEventListener("load", fetchBreedsLogic);
+breedSelect.addEventListener("change", fetchCatByBreedLogic);
 
-export function fetchBreeds(selector, loaderElem, errorElem) {
-    fetch(breedUrl, {headers: {"x-api-key": api_key}})
-        .then((response) => {
-            selector.classList.replace("visible", "hidden");
-            loaderElem.classList.replace("hidden", "visible");
-            errorElem.classList.replace("visible", "hidden");
+export function fetchBreedsLogic() {
+    breedSelect.classList.replace("visible", "hidden");
+    loaderEl.classList.replace("hidden", "visible");
+    errorEl.classList.replace("visible", "hidden");
 
-            return response.json();
-        })
+    fetchBreeds()
         .then((breeds) => {
-            selector.classList.replace("hidden", "visible");
-            loaderElem.classList.replace("visible", "hidden");
-            errorElem.classList.replace("visible", "hidden");
+            breedSelect.classList.replace("hidden", "visible");
+            loaderEl.classList.replace("visible", "hidden");
+            errorEl.classList.replace("visible", "hidden");
      
             breeds.forEach(breed => {
                 let option = document.createElement('option');          
                 option.value = breed.id;
                 option.textContent = breed.name;
-                selector.append(option);
+                breedSelect.append(option);
             });      
         })
         .catch(() => {
-            selector.classList.replace("visible", "hidden");
-            loaderElem.classList.replace("visible", "hidden");
-            errorElem.classList.replace("hidden", "visible");
+            breedSelect.classList.replace("visible", "hidden");
+            loaderEl.classList.replace("visible", "hidden");
+            errorEl.classList.replace("hidden", "visible");
         }
     );
 }
  
-export function fetchCatByBreed(catId) {
-    fetch(catUrl + catId, {headers: {"x-api-key": api_key}})
-       .then((response) => {
-          catInfo.classList.replace("visible", "hidden");
-          loaderEl.classList.replace("hidden", "visible");
-          errorEl.classList.replace("visible", "hidden");
+export function fetchCatByBreedLogic() {
+    catInfo.classList.replace("visible", "hidden");
+    loaderEl.classList.replace("hidden", "visible");
+    errorEl.classList.replace("visible", "hidden");
+
+    fetchCatByBreed(breedSelect.value)
+        .then((data) => {
+            catInfo.classList.replace("hidden", "visible");
+            loaderEl.classList.replace("visible", "hidden");
+            errorEl.classList.replace("visible", "hidden");
  
-          return response.json();
-       })
-       .then((data) => {
-          catInfo.classList.replace("hidden", "visible");
-          loaderEl.classList.replace("visible", "hidden");
-          errorEl.classList.replace("visible", "hidden");
+            const breedData = data[0].breeds[0];
  
-          const breedData = data[0].breeds[0];
- 
-          catInfo.innerHTML = `
-             <div class="cat-info-content">
-             <img class="breed-image" src="https://cdn2.thecatapi.com/images/${breedData.reference_image_id}.jpg" alt="Cat Image">
-             <div class="breed-text-info">
-                <p class="breed-description">${breedData.description}</p>
-                <p class="breed-temperament">${breedData.temperament}</p>
-             </div>
-          `;
-          })
-       .catch(() => {
-          catInfo.classList.replace("visible", "hidden");
-          loaderEl.classList.replace("visible", "hidden");
-          errorEl.classList.replace("hidden", "visible");
-       });
+            catInfo.innerHTML = `
+                <div class="cat-info-content">
+                <img class="breed-image" src="https://cdn2.thecatapi.com/images/${breedData.reference_image_id}.jpg" alt="Cat Image">
+                <div class="breed-text-info">
+                    <p class="breed-description">${breedData.description}</p>
+                    <p class="breed-temperament">${breedData.temperament}</p>
+                </div>
+            `;
+            })
+        .catch(() => {
+            catInfo.classList.replace("visible", "hidden");
+            loaderEl.classList.replace("visible", "hidden");
+            errorEl.classList.replace("hidden", "visible");
+        });
  }
